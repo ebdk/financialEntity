1755208dde4a0ff9ecf41694d6c705210d456fbc
ebdk <edbedecarats@gmail.com>
2020-11-08 07:22:25 -0300

    First Commit

---
+++ src/main/java/com/uade/financialEntity/FinancialEntityApplication.java
@@ -0,0 +1,13 @@
+package com.uade.financialEntity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialEntityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uade/financialEntity/FinancialEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.financialEntity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialEntityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
